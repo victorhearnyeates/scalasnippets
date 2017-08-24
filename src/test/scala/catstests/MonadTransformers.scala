d3e1b775f18e1f49758a6b6993f8cfdc3bf8db27
James Reddick <jamesreddick@James-Reddick-MacBook.local>
2017-08-24 11:09:17 +0100

    Monad Transformer examples.

---
+++ src/test/scala/catstests/MonadTransformers.scala
@@ -0,0 +1,47 @@
+package catstests
+
+import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.concurrent.ScalaFutures
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Try
+
+class MonadTransformers extends FunSuite with Matchers with ScalaFutures {
+
+  test("OptionT") {
+
+    import cats.data.OptionT
+    import cats.instances.future._
+
+    val fo1: Future[Option[Int]] = Future.successful(Option(1))
+
+    val resultOptionT: OptionT[Future, Int] = for {
+      one <- OptionT.apply[Future, Int](fo1)
+      two <- OptionT.pure[Future](2)
+      three <- OptionT.liftF[Future, Int](Future.successful(3))
+    } yield one + two + three
+
+    val resultFo: Future[Option[Int]] = resultOptionT.value
+    resultFo.futureValue should contain (6)
+  }
+
+  test("EitherT") {
+
+    import cats.data.EitherT
+    import cats.instances.future._
+
+    type ResultF[A] = EitherT[Future, String, A]
+
+    val parseInt: Either[String, Int] = Try("1".toInt).toEither.left.map(_ => "Error")
+
+    val result: ResultF[Int] = for {
+      one <- EitherT.fromEither(parseInt)
+      two <- EitherT.pure[Future, String](2)
+      three <- EitherT.right[String](Future.successful(3))
+    } yield one + two + three
+
+    val resultFe: Future[Either[String, Int]] = result.value
+    resultFe.futureValue should === (Right(6))
+  }
+}
