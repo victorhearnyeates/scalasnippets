d0f9556f12c29f001b755f1c35b3f0881226d68f
James Reddick <reddicj@gmail.com>
2017-09-17 16:31:38 +0100

    HList map

--- src/test/scala/shapelesstests/MapHListTest.scala
+++ src/test/scala/shapelesstests/MapHListTest.scala
@@ -8,6 +8,8 @@ class MapHListTest extends FunSuite with Matchers {
 
     import shapeless._
     import syntax.std.tuple._
+    import cats.Show
+    import cats.instances.all._
 
     case class Foo(name: String, age: Int, status: Boolean)
 
@@ -20,10 +22,16 @@ class MapHListTest extends FunSuite with Matchers {
     )
 
     object print extends Poly1 {
-      implicit def default[A]: Case.Aux[(String, A), String] = at(_._1)
+      implicit def default[A](implicit s: Show[A]): Case.Aux[(String, A), (String, String)] =
+        at { case (h, a) => (h, s.show(a)) }
     }
 
-    val headers: List[String] = bar(james).map(print).toList
-    headers should === (List("Name", "Age", "Status"))
+    val headers: List[(String, String)] = bar(james).map(print).toList
+
+    headers should === (List(
+      "Name" -> "James",
+      "Age" -> "45",
+      "Status" -> "true"
+    ))
   }
 }
