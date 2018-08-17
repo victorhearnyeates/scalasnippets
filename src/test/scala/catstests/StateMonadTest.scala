5c20652bffcb7c85ea9d22e650554e5d2558cca8
James Reddick <reddicj@gmail.com>
2018-08-17 07:52:43 +0100

    Shapeless Lens and composition with State

--- src/test/scala/catstests/StateMonadTest.scala
+++ src/test/scala/catstests/StateMonadTest.scala
@@ -1,10 +1,19 @@
 package catstests
 
 import org.scalatest.{FunSuite, Matchers}
+import shapeless._
+import cats.data.State
 
 class StateMonadTest extends FunSuite with Matchers {
 
-  import cats.data.State
+  case class FooBar(s: String, i: Int, b: Baz)
+  case class Baz(ss: String)
+
+  object FooBar {
+    val sLens = lens[FooBar] >> 's
+    val iLens = lens[FooBar] >> 'i
+    val ssLens = lens[FooBar] >> 'b >> 'ss
+  }
 
   object Stack {
 
@@ -32,5 +41,19 @@ class StateMonadTest extends FunSuite with Matchers {
 
     s1.runA(Nil).value should contain (3)
     s1.runS(Nil).value should === (List(2, 1))
+  }
+
+  test("Shapeless Lens") {
+
+    val fooBar = FooBar("s", 0, Baz("ss"))
+
+    val prog: State[FooBar, Unit] = for {
+      _ <- State.modify[FooBar](FooBar.sLens.set(_)("abc"))
+      _ <- State.modify[FooBar](FooBar.iLens.set(_)(123))
+      _ <- State.modify[FooBar](FooBar.ssLens.set(_)("xyz"))
+    } yield ()
+
+    val result: FooBar = prog.runS(fooBar).value
+    result shouldEqual FooBar("abc", 123, Baz("xyz"))
   }
 }
