4e4e796a62e771673242b6147f1ddefb49445c1e
James Reddick <jamesreddick@UK1MAC25.local>
2018-08-23 17:39:25 +0100

    mapTraverseSequential

--- src/test/scala/catstests/TraversableFunctorsTest.scala
+++ src/test/scala/catstests/TraversableFunctorsTest.scala
@@ -11,8 +11,27 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class TraversableFunctorsTest extends FunSuite with Matchers with ScalaFutures {
 
+  def sequentialTraverse[F[_], G[_], A, B](as: F[A])(f: A => G[B])(
+    implicit FT: Traverse[F], FAP: Applicative[F], GM: Monad[G], FBMonoid: Monoid[F[B]]
+  ): G[F[B]] = as.foldLeft[G[F[B]]](GM.pure[F[B]](FBMonoid.empty)) {
+    case (acc, nxt) => acc.flatMap[F[B]](bs => f(nxt).map(b => FBMonoid.combine(bs, b.pure[F])))
+  }
+
   def mapTraverse[F[_], G[_], A, B](fas: G[F[A]])(f: A => G[B])(implicit FT: Traverse[F], GM: Monad[G]): G[F[B]] =
     fas.map(_.traverse(f)).flatten
+
+  def mapTraverseSequential[F[_], G[_], A, B](fas: G[F[A]])(f: A => G[B])(
+    implicit FT: Traverse[F], FAP: Applicative[F], GM: Monad[G], FBMonoid: Monoid[F[B]]
+  ): G[F[B]] = fas.map(as => sequentialTraverse(as)(f)).flatten
+
+
+  test("mapTraverseSequential") {
+
+    mapTraverseSequential[List, Id, Int, Int](List(1, 2, 3))(_ * 2) shouldEqual List(2, 4, 6)
+
+    def f(n: Int): Future[Int] = Future.successful(n)
+    mapTraverseSequential[List, Future, Int, Int](Future.successful(List(1, 2, 3)))(n => f(n * 2)).futureValue shouldEqual List(2, 4, 6)
+  }
 
   test("traverse") {
 
