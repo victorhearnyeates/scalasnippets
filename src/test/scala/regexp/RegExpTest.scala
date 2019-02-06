173ba17eb165d7b45a064056e65290340d526021
James Reddick <james.reddick@imanage.com>
2019-02-06 14:58:36 +0000

    Add regular expression test case

--- src/test/scala/regexp/RegExpTest.scala
+++ src/test/scala/regexp/RegExpTest.scala
@@ -1,10 +1,24 @@
 package regexp
 
+import scala.util.Try
 import scala.util.matching.Regex
 
 import org.scalatest.{FunSuite, Matchers}
 
 class RegExpTest extends FunSuite with Matchers {
+
+  test("Match a substring") {
+
+    val str1 = """ attachment; filename="foo.bar" """.trim
+    val str2 = """ nomatch; filename="foo.bar" """.trim
+    val regexp: Regex = "attachment; filename=\"(.*)\"".r
+
+    val filename1: Option[String] = Try{ val regexp(n) = str1; n }.toOption
+    val filename2: Option[String] = Try{ val regexp(n) = str2; n }.toOption
+
+    filename1 should contain ("foo.bar")
+    filename2 shouldBe empty
+  }
 
   test("Exclude chars from a string") {
 
